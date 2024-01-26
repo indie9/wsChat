125f4f0dc5827a324966f034d1d9ffae08596f25
DESKTOP-O5EKG11\Nikolay <Galanov.N@digdes.com>
2024-01-26 22:00:05 +0300

    updt project

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
       path: "/users",
       name: "users",
       component: () => import("../views/UsersView.vue"),
